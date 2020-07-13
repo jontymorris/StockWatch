41a8b4d4c5ad878553d92c2aa51b802da273d6cd
SillySam <samredmondtech@gmail.com>
2020-07-14 04:11:18 +1200

    Annotated variables and added a few new options

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+# login
 username = "bob@gmail.com"
 password = "password"
-buy_amount = 10
-minimum_stock_price = 1
+
+buy_amount = 10           ''' How much you want to spend on each share '''
+
+minimum_stock_price = 1   ''' The lowest valued stock you're willing to buy '''
+
+sell_profit_margin  = 2   ''' How much profit do you want before selling? (in percentage %) '''
+
+dividends_bonus = 2       ''' Buy amount will increase by this percentage if dividends are upcoming
+                              1 (or less) = no increase '''
